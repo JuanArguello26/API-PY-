60f7e3378c0f6d5ea4b1b7d0b5dcc339cbc88338
Juan Esteban Argüello Botero <juanarguello0526@gmail.com>
2026-03-05 22:03:46 -0500

    feat: implement student CRUD operations with in-memory storage.

--- main.py
+++ main.py
@@ -34,3 +34,48 @@ def calcular_edad(anio: int):
     año_actual = datetime.now().year
     edad = año_actual - anio
     return {"edad": edad}
+
+
+
+#Base de datos en memoria
+
+estudiantes = [
+    {"id": 1, "nombre": "Juan Esteban Argüello Botero"},
+    {"id": 2, "nombre": "Stiven Arevalo Gutierrez"},
+    {"id": 3, "nombre": "Maria Jose Henao Guerrero"},
+]
+
+@app.get("/estudiantes")
+def obtener_estudiantes():
+    return estudiantes
+
+@app.get("/estudiante/{id}")
+def obtener_estudiante(id: int):
+    for estudiante in estudiantes:
+        if estudiante["id"] == id:
+            return estudiante
+    return {"mensaje": "Estudiante no encontrado"}
+
+@app.post("/estudiante")
+def crear_estudiante(estudiante: dict):
+    estudiantes.append(estudiante)
+    return {"mensaje": "Estudiante creado exitosamente", "data": estudiante}
+
+@app.put("/estudiante/{id}")
+def actualizar_estudiante(id: int, estudiante: dict):
+    for estudiante in estudiantes:
+        if estudiante["id"] == id:
+            estudiante["nombre"] = estudiante["nombre"]
+            return {"mensaje": "Estudiante actualizado exitosamente", "data": estudiante}
+    return {"mensaje": "Estudiante no encontrado"}
+
+@app.delete("/estudiante/{id}")
+def eliminar_estudiante(id: int):
+    for estudiante in estudiantes:
+        if estudiante["id"] == id:
+            estudiantes.remove(estudiante)
+            return {"mensaje": "Estudiante eliminado exitosamente"}
+    return {"mensaje": "Estudiante no encontrado"}
+
+
+
